bc756041328e841770b365df902fbc7ced21cab2
Ali nickseresht <53134533+Alinickseresht@users.noreply.github.com>
2026-05-10 15:34:50 +0200

    download_data.sh

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -1,1 +1,7 @@
+#!/bin/bash
+# First install kaggle-cli: pip install kaggle
+# Then set your API key
 
+kaggle datasets download -d your-dataset-id
+unzip your-dataset-id.zip -d data/raw/
+rm your-dataset-id.zip
